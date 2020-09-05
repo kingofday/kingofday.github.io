634628c7d749d8dac560b59921fbb9a8a3301510
Shahrooz <shahrooz.bazrafshan@gmail.com>
2020-09-05 12:38:40 +0430

    pwa8

--- worker.js
+++ worker.js
@@ -22,8 +22,7 @@ self.addEventListener('install', e => {
 
 // Cache and return requests
 self.addEventListener('fetch', e => {
-  alert(e.request.url);
-console.log(`url: ${e.request.url}`);
+  console.log(`url: ${e.request.url}`);
   // console.log('[ServiceWorker] Fetch', e.request.url);
   // if (event.request.mode === 'navigate') {
   //   e.respondWith(caches.match('/index.html'));
@@ -60,44 +59,44 @@ console.log(`url: ${e.request.url}`);
 });
 
 
-  //================================================1
-  // e.respondWith(
-  //   fetch(e.request)
-  //     .then(function (res) {
-  //       let resClone = res.clone();
-  //       caches.open(CACHE_NAME)
-  //         .then(function (cache) {
-  //           if (e.request.url.indexof('https') !== -1)
-  //             cache.put(e.request, resClone);
-  //         });
-  //       return res;
-  //     })
-  //     .catch((err) => {
-  //       console.log(e.request.url);
-  //       return caches.match(e.request).then((res) => res);
-  //     })
+//================================================1
+// e.respondWith(
+//   fetch(e.request)
+//     .then(function (res) {
+//       let resClone = res.clone();
+//       caches.open(CACHE_NAME)
+//         .then(function (cache) {
+//           if (e.request.url.indexof('https') !== -1)
+//             cache.put(e.request, resClone);
+//         });
+//       return res;
+//     })
+//     .catch((err) => {
+//       console.log(e.request.url);
+//       return caches.match(e.request).then((res) => res);
+//     })
 
-  //================================================2
-  //   function () {
-  //   console.log()
-  //   if (event.request.url.indexOf(apiUrl)) {
-  //     if (response) {
-  //       return response;
-  //     }
-  //     return fetch(event.request);
-  //   }
-  // }
+//================================================2
+//   function () {
+//   console.log()
+//   if (event.request.url.indexOf(apiUrl)) {
+//     if (response) {
+//       return response;
+//     }
+//     return fetch(event.request);
+//   }
+// }
 
-  // caches.match(event.request)
-  //   .then(function(response) {
-  //     // Cache hit - return response
-  //     if (response) {
-  //       return response;
-  //     }
-  //     return fetch(event.request);
-  //   }
-  // )
-  //);
+// caches.match(event.request)
+//   .then(function(response) {
+//     // Cache hit - return response
+//     if (response) {
+//       return response;
+//     }
+//     return fetch(event.request);
+//   }
+// )
+//);
 //});
 
 // Update a service worker
